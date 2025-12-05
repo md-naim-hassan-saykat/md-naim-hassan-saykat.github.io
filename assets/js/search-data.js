008c32be0cbda968da70d4d7fa628a5c6f0e6528
md-naim-hassan-saykat <123201256+md-naim-hassan-saykat@users.noreply.github.com>
2025-12-05 09:14:30 +0000

    Deploying to gh-pages from @ md-naim-hassan-saykat/md-naim-hassan-saykat.github.io@a47f34c7f09887389edfacc96ece2aba10ee6bc3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-generalizable-deep-learning-ensemble-for-skin-lesion-classification",
+          title: 'Generalizable Deep Learning Ensemble for Skin Lesion Classification',
+          description: "A state-of-the-art ensemble framework for melanoma and skin lesion diagnosis with cross-dataset validation and explainable AI (Grad-CAM).",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
