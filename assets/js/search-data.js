f598f068f7628bd8fd5607c64f4bd1f1f1f0ae17
md-naim-hassan-saykat <123201256+md-naim-hassan-saykat@users.noreply.github.com>
2026-01-14 16:38:11 +0000

    Deploying to gh-pages from @ md-naim-hassan-saykat/md-naim-hassan-saykat.github.io@54a39932f881757fb26cc0a725b1e6e371b3a86c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-repositories",
+          title: "repositories",
+          description: "Selected GitHub repositories (research, projects, and implementations).",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
