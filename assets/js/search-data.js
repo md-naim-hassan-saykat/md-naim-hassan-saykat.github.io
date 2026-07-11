e8ae340fd9f447d874c62e314742cd4bb072803b
md-naim-hassan-saykat <123201256+md-naim-hassan-saykat@users.noreply.github.com>
2026-07-11 11:15:39 +0000

    Deploying to gh-pages from @ md-naim-hassan-saykat/md-naim-hassan-saykat.github.io@66ac47b0648e5994735eab7acee8f4d16aeea7f7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -71,11 +71,6 @@ ninja.data = [{
           description: "Implementation of a simplified DRAW-style recurrent variational autoencoder for iterative image reconstruction and generation on Fashion-MNIST.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-simplified-draw-model-for-generative-image-modeling",
-          title: 'Simplified DRAW Model for Generative Image Modeling',
-          description: "Implementation of a simplified DRAW-style recurrent variational autoencoder for iterative image reconstruction and generation on Fashion-MNIST.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
